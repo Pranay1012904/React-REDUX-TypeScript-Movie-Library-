c86c4620cdde6ce924994e245015e7f5c10a4665
pranayXoX <pranay.dmnsns@gmail.com>
2022-06-11 18:26:50 +0530

    card work almost over

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -35,6 +35,32 @@ const MovieCard: React.FunctionComponent<movieProp> = ({ movie }) => {
       <div className="row no-gutters">
         <div className="col-md-4 poster-Div">
           <img src={movie.Poster} alt="..." />
+          <div className="cover d-flex">
+            <div className="container tab-a">
+              <div className="row poster-box">
+                <div className="col-6 img-box">
+                  <img src="https://play-lh.googleusercontent.com/hH-oRsfkhbQzVG5CuLRmdAlGonfQIlKa5sfmuT6vMWH0umY2ZpLp6w0zU0jySKdicKk" />
+                </div>
+                <div className="col-6 img-score">
+                  {movie?.Ratings[1]?.Value}
+                </div>
+              </div>
+              <div className="row poster-box mt-1">
+                <div className="col-6 img-box">
+                  <img src="https://iconape.com/wp-content/png_logo_vector/metacritic-logo-original.png" />
+                </div>
+                <div className="col-6 img-score">
+                  {movie?.Ratings[2]?.Value}
+                </div>
+              </div>
+              <div className="row poster-box mt-1">
+                <div className="col-6 img-box">
+                  <img src="https://icons.iconarchive.com/icons/danleech/simple/1024/imdb-icon.png" />
+                </div>
+                <div className="col-6 img-score">{movie?.imdbRating}</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-md-8">
           <div className="card-body">
